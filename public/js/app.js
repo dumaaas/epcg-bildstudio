ddab60acb38aea3d4efc5ab05aa00f042fa1b131
dumaaas <markodumnic8@gmail.com>
2021-03-23 16:51:19 +0100

    Finished first item of slider 3 - mobile

--- public/js/app.js
+++ public/js/app.js
@@ -211,7 +211,7 @@ $(document).ready(function () {
         responsive: {
             0: {
                 navText: ["", "<svg class='mx-auto' xmlns='http://www.w3.org/2000/svg' x='0px' y='0px' width='42' height='42' viewBox='0 0 172 172' style=' fill:#000000;'><g fill='none' fill-rule='nonzero' stroke='none' stroke-width='1' stroke-linecap='butt' stroke-linejoin='miter' stroke-miterlimit='10' stroke-dasharray='' stroke-dashoffset='0' font-family='none' font-weight='none' font-size='none' text-anchor='none' style='mix-blend-mode: normal'><path d='M0,172v-172h172v172z' fill='none'></path><g fill='#2bb7e5'><path d='M113.88281,49.88672l-7.55859,7.72656l23.01172,23.01172h-107.83594v10.75h107.83594l-23.01172,23.01172l7.55859,7.72656l36.11328,-36.11328z'></path></g></g></svg>"],
-
+                margin: 0,
             },
             // 1024: {
             //     stagePadding: 300,
